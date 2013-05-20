7dfe034be42c0350adacd33c8cc96468393b941d
Ryan Souza <rsouza@engineyard.com>
2013-05-19 22:09:34 -0700

    Validate List name and title

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,4 @@
 class List < ActiveRecord::Base
+  validates_presence_of :name, :title
+  validates_format_of :name, with: /\A[a-zA-Z_\-0-9]+\z/, message: "must contain only alpha-numeric, underscores, and dashes"
 end
