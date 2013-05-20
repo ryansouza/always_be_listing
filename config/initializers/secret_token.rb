e25ab4bdb3b2f215ef840d0f08552901eac0782b
Ryan Souza <rsouza@engineyard.com>
2013-05-19 18:53:34 -0700

    Hide app secret key, add bin/bootstrap

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-AlwaysBeLists::Application.config.secret_key_base = '780566721328477c252a766d51d3e4dceacdb8dd4fa9602a6cd1126afe4b5aa7eeb998c53a5a27e8d2c317bb6ac473e0d22a6903f85b28e12887347ceb7068aa'
+AlwaysBeLists::Application.config.secret_key_base = ENV['APP_SECRET_TOKEN']
