3fd65f21b8c40b1d07decdb77a5a7bb2e7f9c097
Ryan Souza <rsouza@engineyard.com>
2013-05-19 19:51:29 -0700

    Fix things and render /

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 AlwaysBeLists::Application.routes.draw do
   resources :lists
 
+  root 'application#root'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
