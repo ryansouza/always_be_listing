16bbf2699117616e93535edc462bb761c5ad1094
Ryan Souza <rsouza@engineyard.com>
2013-05-19 18:33:56 -0700

    Base Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlwaysBeLists::Application.config.secret_key_base = '780566721328477c252a766d51d3e4dceacdb8dd4fa9602a6cd1126afe4b5aa7eeb998c53a5a27e8d2c317bb6ac473e0d22a6903f85b28e12887347ceb7068aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AlwaysBeLists::Application.config.session_store :cookie_store, key: '_always_be_lists_session'
